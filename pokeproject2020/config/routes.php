c7bc0b03149b26dcecc9c2ab4f97446c5cf77868
hashgrem <jeremydemard@gmail.com>
2021-01-07 21:50:53 +0100

    Affichage des pokemons à la racine

--- pokeproject2020/config/routes.php
+++ pokeproject2020/config/routes.php
@@ -55,8 +55,9 @@ $routes->scope('/', function (RouteBuilder $builder) {
      * its action called 'display', and we pass a param to select the view file
      * to use (in this case, templates/Pages/home.php)...
      */
-    $builder->connect('/', ['controller' => 'Pages', 'action' => 'display', 'home']);
-
+    // $builder->connect('/', ['controller' => 'Pages', 'action' => 'display', 'home']);
+    //affichage des pokemons sur la home page
+    $builder->connect('/', ['controller' => 'Pokemons', 'action' => 'index']);
     /*
      * ...and connect the rest of 'Pages' controller's URLs.
      */
@@ -85,10 +86,10 @@ $routes->scope('/', function (RouteBuilder $builder) {
  * ```
  * $routes->scope('/api', function (RouteBuilder $builder) {
  *     // No $builder->applyMiddleware() here.
- *     
+ *
  *     // Parse specified extensions from URLs
  *     // $builder->setExtensions(['json', 'xml']);
- *     
+ *
  *     // Connect API actions here.
  * });
  * ```
