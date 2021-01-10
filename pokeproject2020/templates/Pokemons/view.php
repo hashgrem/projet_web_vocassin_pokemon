9ffda4316801816900a9f20ece6206fe4f6c066d
hashgrem <jeremydemard@gmail.com>
2021-01-10 20:16:09 +0100

    Mise en place de la maquette

--- pokeproject2020/templates/Pokemons/view.php
+++ pokeproject2020/templates/Pokemons/view.php
@@ -8,7 +8,7 @@
 <div class="row">
     <aside class="column">
         <div class="side-nav">
-            <h4 class="heading"><?= __('Actions') ?></h4>
+            <p class="heading"><?= __('Actions') ?></p>
             <?= $this->Form->postLink(__('Delete Pokemon'), ['action' => 'delete', $pokemon->id], ['confirm' => __('Are you sure you want to delete # {0}?', $pokemon->id), 'class' => 'side-nav-item']) ?>
             <?= $this->Html->link(__('List Pokemons'), ['action' => 'index'], ['class' => 'side-nav-item']) ?>
         </div>
@@ -16,16 +16,32 @@
     <div class="column-responsive column-80">
         <div class="pokemons view content">
             <h3><?= h($pokemon->name) ?></h3>
+            <?php
+            //Affichage des 2 types
+            foreach ($pokemon->pokemon_types as $pokemonTypes)
+              {
+
+               ?>
+              <tr>
+                <td>
+
+                  <h4 class="type"><?php  echo $pokemonTypes['type']['name']; ?></h4>
+
+                </td>
+
+              </tr>
+              <?php
+            }
+            ?>
+
             <table>
                 <?php
-                echo "debut de table \n";
-
 
                 if (!empty($pokemon->pokemon_stats))
                  {
                  //$name = ["pokemon" => ["pokemon_stats"] => ["stat"] => 'name'];?>
                  <div class="table-responsive">
-                    <table>
+
                       <?php foreach ($pokemon->pokemon_stats as $pokemonStats)
                         {
 
@@ -45,85 +61,134 @@
                     }
                  ?>
               </table>
+
+              <?php
+              //Affichage de l'image
+              $url = $pokemon['default_front_sprite_url'];
+              ?>
+              <img class="default_front" src="<?php echo $url; ?>">
+
+              <?php
+
+              //Carousel:
+
+              $url = $pokemon['default_front_sprite_url'];
+              $url_back = $pokemon['default_back_sprite_url'];
+              $url_shiny = $pokemon['front_shiny_url'];
+              ?>
+              <div class="slider">
+                 <!-- Fleche droite <i id="arrow_right" class="fas fa-arrow-right"></i> -->
+                <figure>
+                  <img class="carousel" id="front" src="<?php echo $url; ?>">
+                  <img class="carousel" id="shiny" src="<?php echo $url_shiny; ?>">
+                  <img class="carousel" id="back"  src="<?php echo $url_back; ?>">
+
+                </figure>
+
+              </div>
+              <!-- fin Carousel -->
+
             </div>
           </div>
 
 
 
-          <?php
-          //Affichage de l'image
-          $url = $pokemon['default_front_sprite_url'];
-          ?>
-          <img src="<?php echo $url; ?>">
-
-          <?php
-          //Affichage des 2 types
-          foreach ($pokemon->pokemon_types as $pokemonTypes)
-            {
-
-             ?>
-            <tr>
-              <td>
-                <?php
-                  echo $pokemonTypes['type']['name'];
-                ?>
-              </td>
-
-            </tr>
-            <?php
-          }
-
-          //Carousel:
-          echo "Carousel: \n";
-          $url = $pokemon['default_front_sprite_url'];
-          $url_back = $pokemon['default_back_sprite_url'];
-          $url_shiny = $pokemon['front_shiny_url'];
-          ?>
-          <img src="<?php echo $url; ?>">
-          <img src="<?php echo $url_back; ?>">
-          <img src="<?php echo $url_shiny; ?>">
 
 
 
 
 
+<!--
+
+  En modifiant, style.css -> aucun changement
+  En modifiant, stlye.scss -> aucun changement
+  En linkant un fichier .css ou .scss aucun changement
+  En vidant le cache de mon navigateur -> aucun changement..
+
+  Aucune méthode ne marche, seule solution: faire le style ici.. :/
+
+-->
+
+<style>
+ h3 {
+     text-align: center;
+     text-decoration: underline;
+     letter-spacing: 0.05em;
+     padding: 10px;
+
+
+ }
+
+
+ table {
+
+   display: inline-block;
+   width: 250px;
+   margin-left:50%;
+   margin-top: 30px;
+   border: 1px solid;
+   padding:30px;
+
+ }
 
 
 
 
+.default_front {
+   display: inline;
+   margin-left: 15%;
+   margin-top:-67%;
+   border: solid 1px;
+   border-radius: 50%;
+   width: 150px;
+ }
 
 
-            <div class="related">
-                <h4><?= __('Related Pokemon Types') ?></h4>
-                <?php if (!empty($pokemon->pokemon_types)) : ?>
-                <div class="table-responsive">
-                    <table>
-                        <tr>
-                            <th><?= __('Id') ?></th>
-                            <th><?= __('Pokemon Id') ?></th>
-                            <th><?= __('Type Id') ?></th>
-                            <th><?= __('Created') ?></th>
-                            <th><?= __('Modified') ?></th>
-                            <th class="actions"><?= __('Actions') ?></th>
-                        </tr>
-                        <?php foreach ($pokemon->pokemon_types as $pokemonTypes) : ?>
-                        <tr>
-                            <td><?= h($pokemonTypes->id) ?></td>
-                            <td><?= h($pokemonTypes->pokemon_id) ?></td>
-                            <td><?= h($pokemonTypes->type_id) ?></td>
-                            <td><?= h($pokemonTypes->created) ?></td>
-                            <td><?= h($pokemonTypes->modified) ?></td>
-                            <td class="actions">
-                                <?= $this->Html->link(__('View'), ['controller' => 'PokemonTypes', 'action' => 'view', $pokemonTypes->id]) ?>
-                                <?= $this->Html->link(__('Edit'), ['controller' => 'PokemonTypes', 'action' => 'edit', $pokemonTypes->id]) ?>
-                                <?= $this->Form->postLink(__('Delete'), ['controller' => 'PokemonTypes', 'action' => 'delete', $pokemonTypes->id], ['confirm' => __('Are you sure you want to delete # {0}?', $pokemonTypes->id)]) ?>
-                            </td>
-                        </tr>
-                        <?php endforeach; ?>
-                    </table>
-                </div>
-                <?php endif; ?>
-            </div>
-        </div>
-    </div>
-</div>
+.type{
+
+  font-family: "Open Sans Condensed", "Open Sans", helvetica, sans-serif;
+  letter-spacing: 0.1em;
+  padding: 10px;
+  border-radius: 10%;
+  border: 1px solid;
+  display: inline-block;
+  margin-top: 50px;
+  margin-left: 50%;
+  margin-right: -40%;
+  background: linear-gradient(to bottom right, #FD2A2A, #F79797);
+
+
+ }
+
+.carousel {
+  border: 1px solid;
+  border-radius: 50%;
+  width: 150px;
+  margin-top: 50px;
+  margin-bottom: 80px;
+}
+
+#back {
+  background-color: grey;
+  position: relative;
+  margin-left: -250px;
+
+}
+
+#front , #shiny {
+  background-color: #BBBAB9;
+}
+
+#front {
+  margin-left: 30%;
+}
+
+#shiny {
+  margin-left: 40px;
+
+}
+
+
+
+
+</style>
